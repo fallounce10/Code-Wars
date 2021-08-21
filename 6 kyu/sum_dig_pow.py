3dd1cefb869279472cbe2db9f7e967bf11824e6d
fallounce10 <pinhal.silva96@gmail.com>
2021-08-21 10:34:39 +0100

    Added sum_dig_pow.py

---
+++ 6 kyu/sum_dig_pow.py
@@ -0,0 +1,12 @@
+def sum_dig_pow(a, b):
+	arr = []
+
+	for x in range(a, b+1):
+		char = []
+		n = 0
+		for i in str(x):
+			char.append(int(i) ** (n+1))
+			n += 1
+		if x == sum(char)
+			arr.append(x)
+	return arr
